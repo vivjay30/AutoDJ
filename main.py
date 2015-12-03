10115a80d38382b39fbc3d4437226b8aa64d9c3d
Vivek Jayaram <vivekjayaram@college.harvard.edu>
2015-12-03 00:40:23 -0500

    Basic mixing working

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import itertools
 # This is a dict of dicts storing how well key1 mashes out into key2
 MASHABILITY_DICT = {}
 
+# This is the number of beats we wish to transition, should be a multiple of 4
+# Mix in and mix out points have to be set accordingly
+MIX_LENGTH = 32
+
 class Song(object):
 	def __init__(self, name, fname, mix_in, mix_out, unary_factor, song_id, bpm):
 		self.name = name
@@ -18,6 +22,7 @@ class Song(object):
 		self.unary_factor = unary_factor
 		self.song_id = song_id
 		self.bpm = bpm
+		self.AudioFile = audio.LocalAudioFile("mp3/" + fname)
 
 def mashability(song1, song2):
 	"""
@@ -32,8 +37,40 @@ def renderList(songList, outFile):
 	"""
 	Takes a list of songs and outputs them to outFile
 	Has to beatmatch and cross fade
+	Assumes songList >= 2
 	"""
-	pass
+	mixOutSeg = None
+	currAudio = None
+	for i in range(len(songList)):
+		currSong = songList[i]
+		currBeats = currSong.AudioFile.analysis.beats
+		# This happens on the first iteration, nothing to mix in so just play until mix out
+		if not mixOutSeg:
+			currAudio = audio.getpieces(
+							currSong.AudioFile,
+							currBeats[currSong.mix_in:currSong.mix_out]
+			)
+		else:
+			mixInSeg = audio.getpieces(
+							currSong.AudioFile,
+							currBeats[currSong.mix_in:currSong.mix_in+MIX_LENGTH])
+			transition_length = 60.0/128.0 * MIX_LENGTH 
+			mixInSeg.encode("Mixinseg.mp3")
+			mixOutSeg.encode("MixOutseg.mp3")
+			print transition_length
+			transitionSeg = action.Crossfade([mixOutSeg, mixInSeg], [0.0, 0.0], transition_length).render()
+			transitionSeg.encode("transition.mp3")
+			#return
+			mainSeg = audio.getpieces(
+							currSong.AudioFile,
+							currBeats[currSong.mix_in+MIX_LENGTH:currSong.mix_out])
+			currAudio = audio.assemble([currAudio, transitionSeg, mainSeg])
+
+		mixOutSeg = audio.getpieces(
+						currSong.AudioFile,
+						currBeats[currSong.mix_out:currSong.mix_out+MIX_LENGTH])
+	#currAudio = audio.assemble([currAudio, mixOutSeg])
+	currAudio.encode(outFile)
 
 
 def main():
