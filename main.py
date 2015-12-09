574a2f6b91e304cb49564d0c04c306a1871a99c1
Vivek Jayaram <vivekjayaram@college.harvard.edu>
2015-12-09 01:55:02 -0500

    Added cross bpm functionality

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import random
 import librosa
 import numpy as np
 import scipy.spatial.distance as dst
+import matplotlib.pyplot as plt
 
 config.ECHO_NEST_API_KEY = "O63DZS2LNNEWUO9GC"
 
@@ -19,6 +20,7 @@ MASHABILITY_DICT = {}
 # This is the number of beats we wish to transition, should be a multiple of 4
 # Mix in and mix out points have to be set accordingly
 MIX_LENGTH = 32
+NUM_SONGS = 4
 
 class Song(object):
 	def __init__(self, name, fname, mix_in, mix_out, unary_factor, song_id, bpm):
@@ -40,11 +42,10 @@ def get_mash_pairs(songs):
 	mash_dict = {}
 	for song1 in songs:
 		for song2 in songs:
-			if song1 != song2:
+			if song1 != song2 and abs(song1.bpm - song2.bpm) < 30:
 				mash_dict[(song1, song2)] = mashability(song1, song2)
 	print mash_dict
 	return mash_dict
-
 
 # takes a list of two song id and returns a number between zero and 1
 def mashability(song1, song2):
@@ -59,15 +60,15 @@ def mashability(song1, song2):
 	data2 = audio.getpieces(song2.AudioFile, beats2)
 	data1.encode("temp1.mp3")
 	data2.encode("temp2.mp3")
-	print "Done encoding"
 	y1, sr1 = librosa.load("temp1.mp3")
 	y2, sr2 = librosa.load("temp2.mp3")
-	print "Done loading"
 	S1 = np.abs(librosa.stft(y1, n_fft = 4096))
 	chroma1 = librosa.feature.chroma_stft(S=S1, sr=sr1)
 	S2 = np.abs(librosa.stft(y2, n_fft = 4096))
 	chroma2 = librosa.feature.chroma_stft(S=S2, sr=sr2)
-
+	im = librosa.display.specshow(chroma1,x_axis = "time",y_axis = "chroma")
+	im2 = librosa.display.specshow(chroma2,x_axis = "time",y_axis = "chroma")
+	# plt.show()
 	orthogonal_arr = []
 	for i in range(min(chroma1.shape[1],chroma2.shape[1])):
 		orthogonal_arr.append(dst.cosine(chroma1[:,i],chroma2[:,i]))
@@ -83,6 +84,59 @@ def get_unary_list(songs):
 		ret[song.song_id] = song.unary_factor
 	return ret
 
+def makeTransition(inData, outData, inSong, outSong):
+	"""
+	Takes two arrays of AudioQuantum objects and returns a single AudioData object
+	with a linear crossfade and a beatmatch
+	"""
+	# The number of beats has to be the same
+	assert(len(inData) == len(outData))
+
+	# If the tempos are the same then it is easy
+	if inSong.bpm == outSong.bpm:
+		mixInSeg = audio.getpieces(
+			inSong.AudioFile,
+			inData
+		)
+		mixOutSeg = audio.getpieces(
+			outSong.AudioFile,
+			outData
+		)
+		mixInSeg.encode("outfiles/Mixinseg.mp3")
+		mixOutSeg.encode("outfiles/MixOutseg.mp3")
+		transition_length = 60.0/128.0 * MIX_LENGTH 
+		transitionSeg = action.Crossfade([mixOutSeg, mixInSeg], [0.0, 0.0], transition_length, mode="exponential").render()
+		return transitionSeg
+
+	# Else we iterate over each one
+	else:
+		transitionTime = 0
+		tempoDiff = inSong.bpm - outSong.bpm
+		marginalInc = float(tempoDiff) / float(MIX_LENGTH)
+		inCollect = []
+		outCollect = []
+		# Rather than a linear slowdown, it is a step function where each beat slows down by marginalInc
+		for i in range(MIX_LENGTH):
+			inAudio = inData[i].render()
+			outAudio = outData[i].render()
+			# We scale the in and out beats so that they are at the same temp
+			inScaled = dirac.timeScale(inAudio.data, inSong.bpm / (outSong.bpm + i * marginalInc))
+			outScaled = dirac.timeScale(outAudio.data, outSong.bpm / (outSong.bpm + i * marginalInc))
+			transitionTime += 60 / (outSong.bpm + i * marginalInc)
+			ts = audio.AudioData(ndarray=inScaled, shape=inScaled.shape, 
+				sampleRate=inSong.AudioFile.sampleRate, numChannels=inScaled.shape[1])
+			ts2 = audio.AudioData(ndarray=outScaled, shape=outScaled.shape, 
+				sampleRate=outSong.AudioFile.sampleRate, numChannels=outScaled.shape[1])
+			inCollect.append(ts)
+			outCollect.append(ts2)
+		# Collect the audio and crossfade it
+		mixInSeg = audio.assemble(inCollect, numChannels=2)
+		mixOutSeg = audio.assemble(outCollect, numChannels=2)
+		mixInSeg.encode("outfiles/Mixinseg.mp3")
+		mixOutSeg.encode("outfiles/MixOutseg.mp3")
+		transitionSeg = action.Crossfade([mixOutSeg, mixInSeg], [0.0, 0.0], transitionTime, mode="exponential").render()
+		return transitionSeg
+
 def renderList(songList, outFile):
 	"""
 	Takes a list of songs and outputs them to outFile
@@ -91,6 +145,7 @@ def renderList(songList, outFile):
 	"""
 	mixOutSeg = None
 	currAudio = None
+	prevSong = None
 	for i in range(len(songList)):
 		currSong = songList[i]
 		currBeats = currSong.AudioFile.analysis.beats
@@ -101,25 +156,17 @@ def renderList(songList, outFile):
 							currBeats[currSong.mix_in:currSong.mix_out]
 			)
 		else:
-			mixInSeg = audio.getpieces(
-							currSong.AudioFile,
-							currBeats[currSong.mix_in:currSong.mix_in+MIX_LENGTH])
-			transition_length = 60.0/128.0 * MIX_LENGTH 
-			mixInSeg.encode("Mixinseg.mp3")
-			mixOutSeg.encode("MixOutseg.mp3")
-			print transition_length
-			transitionSeg = action.Crossfade([mixOutSeg, mixInSeg], [0.0, 0.0], transition_length, mode="exponential").render()
-			transitionSeg.encode("transition.mp3")
-			#return
+			mixInSeg = currBeats[currSong.mix_in:currSong.mix_in+MIX_LENGTH]
+			transitionSeg = makeTransition(mixInSeg, mixOutSeg, currSong, prevSong)
+			transitionSeg.encode("outfiles/transition.mp3")
 			mainSeg = audio.getpieces(
 							currSong.AudioFile,
 							currBeats[currSong.mix_in+MIX_LENGTH:currSong.mix_out])
 			currAudio = audio.assemble([currAudio, transitionSeg, mainSeg])
+		mixOutSeg = currBeats[currSong.mix_out:currSong.mix_out+MIX_LENGTH]
+		prevSong = currSong
 
-		mixOutSeg = audio.getpieces(
-						currSong.AudioFile,
-						currBeats[currSong.mix_out:currSong.mix_out+MIX_LENGTH])
-	#currAudio = audio.assemble([currAudio, mixOutSeg])
+	# currAudio = audio.assemble([currAudio, mixOutSeg])
 	currAudio.encode(outFile)
 
 def main():
@@ -139,23 +186,23 @@ def main():
 			bpm=song['BPM']
 		)
 		songs.append(new_song)
-	print "Here"
 	# Determine all pairwise mashabilities, update the dict
-	for song1 in songs:
-		new_dict = {}
-		for song2 in songs:
-			print "Here"
-			if song1 != song2:
-				new_dict[song2] = mashability(song1, song2)
-		MASHABILITY_DICT[song1] = new_dict
+	# for song1 in songs:
+	# 	new_dict = {}
+	# 	for song2 in songs:
+	# 		# Don't consider songs that differ too greatly 
+	# 		if song1 != song2 and abs(song1.bpm - song2.bpm) < 30:
+	# 			new_dict[song2] = mashability(song1, song2)
+	# 	MASHABILITY_DICT[song1] = new_dict
 
 	from csp import generateMix
 	# Use a CSP to solve generate a list of songs
-	mixList = generateMix(songs, 2, get_mash_pairs(songs), get_unary_list(songs))
+	mixList = generateMix(songs, NUM_SONGS, get_mash_pairs(songs), get_unary_list(songs))
 
-	print([song.name for song in mixList])
+	#print([song.name for song in mixList])
 	# Actually render the list to a file
-	renderList(mixList, "MixOut.mp3")
+	#renderList(songs, "outfiles/MixOut.mp3")
+	renderList(mixList, "outfiles/MixOut.mp3")
 
 if __name__ == "__main__":
 	main()
